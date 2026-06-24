e1f9d25db9f18a1733fa0abbb399d2f44d6b590f
marcinoPlayGames <marcin.pawlowski1@microsoft.wsei.edu.pl>
2026-06-24 22:54:29 +0200

    Making main python code for the bot

---
+++ projekt/main.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+
+from config import DISCORD_TOKEN
+
+
+class DiscordBot(commands.Bot):
+
+    def __init__(self):
+
+        intents = discord.Intents.default()
+        intents.members = True
+
+        super().__init__(
+            command_prefix="!",
+            intents=intents
+        )
+
+    async def setup_hook(self):
+
+        await self.load_extension(
+            "bot.cogs.advisor"
+        )
+
+        await self.load_extension(
+            "bot.cogs.moderation"
+        )
+
+        await self.tree.sync()
+
+
+bot = DiscordBot()
+
+bot.run(DISCORD_TOKEN)
