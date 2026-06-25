f242ea7d39c80c8392c6e177ea1da58cde466286
marcinoPlayGames <marcin.pawlowski1@microsoft.wsei.edu.pl>
2026-06-25 20:27:19 +0200

    Preparing database and making code for database logging server moderators actions to bot

---
+++ projekt/services/database.py
@@ -0,0 +1,78 @@
+import sqlite3
+from datetime import datetime
+
+
+class Database:
+
+
+    def __init__(self):
+
+        self.connection = sqlite3.connect(
+            "data/decisions.db"
+        )
+
+        self.create_table()
+
+
+
+    def create_table(self):
+
+        cursor = self.connection.cursor()
+
+        cursor.execute(
+            """
+            CREATE TABLE IF NOT EXISTS decisions (
+
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+                moderator TEXT,
+
+                question TEXT,
+
+                response TEXT,
+
+                date TEXT
+
+            )
+            """
+        )
+
+
+        self.connection.commit()
+
+
+
+    def save_decision(
+        self,
+        moderator,
+        question,
+        response
+    ):
+
+        cursor = self.connection.cursor()
+
+
+        cursor.execute(
+            """
+            INSERT INTO decisions
+            (
+                moderator,
+                question,
+                response,
+                date
+            )
+
+            VALUES (?, ?, ?, ?)
+
+            """,
+
+            (
+                moderator,
+                question,
+                response,
+                datetime.now().isoformat()
+            )
+        )
+
+
+        self.connection.commit()
